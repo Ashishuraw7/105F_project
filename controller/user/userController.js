9a38e0905cd3b77b669b4d047c603413ddcef0f1
Ashishuraw7 <ashux46@gmail.com>
2025-07-27 22:58:52 +0545

    add login

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -14,5 +14,35 @@ exports.registerUser = async(req,res)=>{
         email : email,
         password : bcrypt.hashSync(password,12)
     })
-    res.send("User Registered Successfully")
+    res.redirect("/login")
+}
+
+exports.renderLoginForm = (req,res)=>{
+    res.render("login")
+}
+
+exports.loginUser = async(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.send("please provide email and passwords")
+    }
+
+    // check whether the coming email user exist or not
+    const user = await users.findAll({
+        where : {
+            email : email
+        }
+    })
+    if(user.length ==0){
+        res.send("No user exist with that email")
+    }else{
+        // tyo email ko user xa vanney bujyo --> password pni check garney
+        const isMatched = await bcrypt.compare(password,user[0].password)
+        if (isMatched){
+            res.send("login successfully")
+        }else{
+            res.send("email or passwod is invalid") 
+        }
+    }
+
 }
